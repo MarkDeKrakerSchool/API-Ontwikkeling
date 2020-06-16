b66e7c32c7d6b569875a7ac65b22b71b97c779ab
Mark <mark.de.kraker@outlook.com>
2020-06-16 11:00:54 +0200

    New file

---
+++ kentekencheck.py
@@ -0,0 +1,12 @@
+#
+# import pandas as pd
+# from sodapy import Socrata
+#
+# client = Socrata("opendata.rdw.nl", None)
+# kenteken = str(input("Wat is je kenteken?         "))
+# results = client.get("m9d7-ebf2", "?kenteken=" + kenteken)
+# results_df = pd.DataFrame.from_records(results)
+# print(results)
+
+
+
