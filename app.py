6a56b511a836c3b3b872f62f505366ef04f00984
Lex <vanoslex7@gmail.com>
2020-06-22 12:32:53 +0200

    Added form field

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from kentekencheck import KentekenForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '22e6d440e93bc19fe477da8288560673'
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
 
 @app.route('/search_result')
 def search_result():
     return render_template('search_result.html')
 
+
+@app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    form = KentekenForm()
+    if form.validate_on_submit():
+        return redirect(url_for('search_result'))
+    return render_template('index.html', title='Register', form=form)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
--- kentekencheck.py
+++ kentekencheck.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
+
 class KentekenForm(FlaskForm):
-    kenteken = StringField('Kenteken', validators=[DataRequired(), Length(min=10, max=20)])
+    kenteken = StringField('Kenteken', validators=[DataRequired(), Length(min=5, max=20)])
+    submit = SubmitField('Zoek resultaten')
 
 # import pandas as pd
 # from sodapy import Socrata
