82d4e72849814e6835ba8069bf1b3b56ecd177b5
Mark <mark.de.kraker@outlook.com>
2020-06-22 13:14:50 +0200

    Fixes

--- kentekencheck.py
+++ kentekencheck.py
@@ -7,14 +7,14 @@ class KentekenForm(FlaskForm):
     kenteken = StringField('Kenteken', validators=[DataRequired(), Length(min=5, max=20)])
     submit = SubmitField('Zoek resultaten')
 
-# import pandas as pd
-# from sodapy import Socrata
-#
-# client = Socrata("opendata.rdw.nl", None)
-# kenteken = str(input("Wat is je kenteken?         "))
-# results = client.get("m9d7-ebf2", "?kenteken=" + kenteken)
-# results_df = pd.DataFrame.from_records(results)
-# print(results)
-#
-#
-#
+import pandas as pd
+from sodapy import Socrata
+
+client = Socrata("opendata.rdw.nl", None)
+kenteken = str(input("Wat is je kenteken? "))
+results = client.get("m9d7-ebf2", "?kenteken=" + kenteken)
+results_df = pd.DataFrame.from_records(results)
+print(results)
+
+
+
